566c3ac7310ee7e39411a66baa8029c35460ce8a
Mark42 <dc7303@gmail.com>
2019-01-27 20:53:06 +0900

    react-google-charts api 적용하여 calendar chart sample 구현완료.

--- src/component/graph/graph.js
+++ src/component/graph/graph.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Chart from 'react-google-charts';
 
 const defaultProps = {};
 
@@ -9,11 +10,67 @@ class Graph extends Component {
     super(props);
   }
 
+  options = {
+    calendar: {
+      height: 200,
+      width: 1200,
+      dayOfWeekLabel: {
+        fontName: 'Times-Roman',
+        fontSize: 12,
+        color: 'orange',
+        bold: false,
+        italic: false
+      },
+      focusedCellColor: {
+        stroke: 'red',
+        strokeOpacity: 0.8,
+        strokeWidth: 3
+      },
+      monthLabel: {
+        fontSize: 12,
+        color: 'orange',
+        bold: true,
+        italic: false
+      },
+      monthOutlineColor: {
+        stroke: 'red',
+        strokeOpacity: 0.5,
+        strokeWidth: 2
+      }
+    },
+    colorAxis: {
+      minValue: 0,
+      maxValue: 100000,
+      colors: ['white', 'green']
+    },
+    noDataPattern: {
+      backgroundColor: '#9da7b7',
+      color: '#9da7b7'
+    }
+  };
+
   render() {
     return (
       <div className="header">
-        <h1>Layout Test </h1>
-        <h3>header</h3>
+        <div>
+          <Chart
+            chartType="Calendar"
+            loader={<div>Loading Chart</div>}
+            data={[
+              [
+                { type: 'date', id: 'Date' },
+                { type: 'number', id: 'Won/Loss' }
+              ],
+              [new Date(2012, 3, 13), 70000],
+              [new Date(2012, 3, 14), 10000],
+              [new Date(2012, 3, 15), 30000],
+              [new Date(2012, 3, 16), 45612],
+              [new Date(2012, 3, 17), 144400]
+            ]}
+            options={this.options}
+            rootProps={{ 'data-testid': '1' }}
+          />
+        </div>
       </div>
     );
   }
